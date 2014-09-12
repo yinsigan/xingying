bb2e7909da745eabeb0635d6f1a560bc99489751
yinsigan <ganweiliang886@gmail.com>
2014-09-12 15:06:32 +0800

    change production about email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:   "smtp.gmail.com",
     port:  587,
-    #domain: "domain.of.sender.net",
-    domain: "xingying.herokuapp.com",
+    domain: "domain.of.sender.net",
+    #domain: "xingying.herokuapp.com",
     authentication: "plain",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
