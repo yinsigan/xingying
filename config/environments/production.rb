2c26f6e8382ed25489a0e9b59be9964bcd8038bc
yinsigan <ganweiliang886@gmail.com>
2014-09-12 15:15:12 +0800

    fit
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,12 +88,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:   "smtp.gmail.com",
     port:  587,
-    domain: "domain.of.sender.net",
-    #domain: "xingying.herokuapp.com",
+    # domain: "domain.of.sender.net",
+    domain: "xingying.herokuapp.com",
     authentication: "plain",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
     enable_starttls_auto: true
   }
-
 end
