2efc3ffaa9baec9bb8cadfbbb93e51718667a057
yinsigan <ganweiliang886@gmail.com>
2014-09-12 14:31:40 +0800

    修改生产环境的邮箱配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'xingying.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:   "smtp.gmail.com",
+    port:  587,
+    #domain: "domain.of.sender.net",
+    domain: "xingying.herokuapp.com",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
+  }
+
 end
