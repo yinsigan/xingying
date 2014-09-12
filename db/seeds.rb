b958b93079e940e72220bdddbd79a892c926ad9a
yinsigan <ganweiliang886@gmail.com>
2014-09-12 13:26:22 +0800

    修改seeds.rb添加默认管理用户

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.new(:email => "903279182@qq.com", :password => "12345678")
+admin_user.save
