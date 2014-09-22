476efd187e3626b75ac5839d87c26be1aadb9dbf
yinsigan <ganweiliang886@gmail.com>
2014-09-22 15:46:31 +0800

    添加flash样式

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,3 +1,13 @@
 $(document).on('click', '.yamm .dropdown-menu', function(e) {
   e.stopPropagation()
 })
+
+$(document).ready(function() {
+  var flash = ['info', 'success', 'danger', 'warning'];
+  for (var key in flash) {
+    var select = '.alert-autocloseable-' + flash[key]
+    if ($(select).length > 0) {
+      $(select).delay(5000).fadeOut();
+    }
+  }
+});
