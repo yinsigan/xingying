937389dcb473472d04bdc63c2a9fcf22ccfaf6c4
yinsigan <ganweiliang886@gmail.com>
2014-09-24 10:21:55 +0800

    public new tp变成select_tag

--- app/models/public.rb
+++ app/models/public.rb
@@ -1,5 +1,7 @@
+# encoding: utf-8
 class Public < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true
+  TP={1 => "订阅号", 2 => "服务号"}
 end
