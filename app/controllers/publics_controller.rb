aca6840e4a3a52feab766eee1824e7a88ad61add
yinsigan <ganweiliang886@gmail.com>
2014-09-29 15:29:10 +0800

    修改flash信息

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -1,30 +1,27 @@
 class PublicsController < SettingsController
   before_action :set_public, only: [:show, :edit, :update, :destroy]
+  before_action :add_index_breadcrumb
 
   # GET /publics
   # GET /publics.json
   def index
-    add_breadcrumb I18n.t("breadcrumbs.public.index"), :publics_path
     @publics = current_user.publics
   end
 
   # GET /publics/1
   # GET /publics/1.json
   def show
-    add_breadcrumb I18n.t("breadcrumbs.public.index"), :publics_path
     add_breadcrumb I18n.t("breadcrumbs.public.show"), :public_path
   end
 
   # GET /publics/new
   def new
-    add_breadcrumb I18n.t("breadcrumbs.public.index"), :publics_path
     add_breadcrumb I18n.t("breadcrumbs.public.new"), :new_public_path
     @public = current_user.publics.build
   end
 
   # GET /publics/1/edit
   def edit
-    add_breadcrumb I18n.t("breadcrumbs.public.index"), :publics_path
   end
 
   # POST /publics
@@ -34,7 +31,7 @@ class PublicsController < SettingsController
 
     respond_to do |format|
       if @public.save
-        format.html { redirect_to @public, notice: 'Public was successfully created.' }
+        format.html { redirect_to @public, flash: {success: "提交成功"} }
         format.json { render :show, status: :created, location: @public }
       else
         format.html { render :new }
@@ -48,7 +45,7 @@ class PublicsController < SettingsController
   def update
     respond_to do |format|
       if @public.update(public_params)
-        format.html { redirect_to @public, notice: 'Public was successfully updated.' }
+        format.html { redirect_to @public, flash: {success: "保存成功"}}
         format.json { render :show, status: :ok, location: @public }
       else
         format.html { render :edit }
@@ -62,7 +59,7 @@ class PublicsController < SettingsController
   def destroy
     @public.destroy
     respond_to do |format|
-      format.html { redirect_to publics_url, notice: 'Public was successfully destroyed.' }
+      format.html { redirect_to publics_url, flash: {success: "删除成功"} }
       format.json { head :no_content }
     end
   end
@@ -76,5 +73,9 @@ class PublicsController < SettingsController
     # Never trust parameters from the scary internet, only allow the white list through.
     def public_params
       params.require(:public).permit(:name, :password, :tp, :image)
+    end
+
+    def add_index_breadcrumb
+      add_breadcrumb I18n.t("breadcrumbs.public.index"), :publics_path
     end
 end
