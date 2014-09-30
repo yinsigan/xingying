9ea84b6afd5fcfb49e14df0c4eeb60ecabab885a
yinsigan <ganweiliang886@gmail.com>
2014-09-30 14:25:30 +0800

    修改整个布局

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -26,6 +26,7 @@ $(document).on("page:change", function() {
   //弹出黑色提示框
   $('button[data-tooltip], a[data-tooltip]').tooltip();
 
+  // 把圆形loading去掉
   NProgress.configure({ showSpinner: false });
 
 });
