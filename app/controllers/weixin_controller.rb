d1f6e3cc367d8d1da3c84ac967e3aefd3d8c61e3
yinsigan <ganweiliang886@gmail.com>
2014-09-30 11:56:09 +0800

    添加微信测试

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -1,2 +1,24 @@
 class WeixinController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_filter :initialize_adapter, :check_weixin_legality, only: [:index, :reply]
+
+  def index
+  end
+
+  def reply
+  end
+
+  protected
+
+    def initialize_adapter
+      @weixin_adapter ||= WeixinAdapter.new(params)
+    end
+
+    def check_weixin_legality
+      check_result = @weixin_adapter.check_weixin_legality
+      valid = check_result.delete(:valid)
+      render check_result if action_name == "index"
+      return valid
+    end
+
 end
