905ed9dc5869b6a3aaeedce658fdf9d26ff6ad49
yinsigan <ganweiliang886@gmail.com>
2014-10-04 15:57:57 +0800

    去掉多余的代码

--- config/initializers/require.rb
+++ config/initializers/require.rb
@@ -1,4 +1,4 @@
-require "#{Rails.root}/lib/weixin/adapter.rb"
-require "#{Rails.root}/lib/weixin/message.rb"
-require "#{Rails.root}/lib/weixin/reply_message.rb"
-require "#{Rails.root}/lib/weixin/reply_message_helper.rb"
+require "lib/weixin/adapter.rb"
+require "lib/weixin/message.rb"
+require "lib/weixin/reply_message.rb"
+require "lib/weixin/reply_message_helper.rb"
