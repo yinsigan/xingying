0e871bc35bb3d1ba379073fa718dd741ce66f077
yinsigan <ganweiliang886@gmail.com>
2014-10-09 10:28:39 +0800

    修改验证

--- app/models/public_account.rb
+++ app/models/public_account.rb
@@ -3,14 +3,15 @@
 class PublicAccount < ActiveRecord::Base
   belongs_to :user
   validates :name, :tp, :image, :user, presence: true
+  validates :default_reply, allow_nil: true, length: { in: 1..600 }
   TP={1 => "订阅号", 2 => "服务号"}
   mount_uploader :image, AvatarUploader
 
   before_create :generate_key
 
   private
-    def generate_key
-      self.weixin_secret_key = SecureRandom.urlsafe_base64(32).downcase
-      self.weixin_token = SecureRandom.hex(12)
-    end
+  def generate_key
+    self.weixin_secret_key = SecureRandom.urlsafe_base64(32).downcase
+    self.weixin_token = SecureRandom.hex(12)
+  end
 end
