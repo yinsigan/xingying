e3f68e25d09a13af349f2683d1d569fa54f13ed6
yinsigan <ganweiliang886@gmail.com>
2014-10-11 09:22:18 +0800

    去除input required样式

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,7 +1,4 @@
 module SettingsHelper
-  def required_icon
-    render "shared/required_icon"
-  end
 
   # 重写面包屑
   def render_breadcrumbs(options = {}, &block)
@@ -13,38 +10,40 @@ module SettingsHelper
   end
 
   private
-  def render_element(element)
-    if element.path == nil
-      content = compute_name(element)
-    else
-      content = self.link_to_unless_current(compute_name(element), compute_path(element), element.options)
-    end
-    if current_page?(compute_path(element))
-      self.content_tag("li", content, class: "active")
-    else
-      self.content_tag("li", content)
-    end
-  end
 
-  def compute_name(element)
-    case name = element.name
-    when Symbol
-      self.send(name)
-    when Proc
-      name.call(self)
-    else
-      name.to_s
+    def render_element(element)
+      if element.path == nil
+        content = compute_name(element)
+      else
+        content = self.link_to_unless_current(compute_name(element), compute_path(element), element.options)
+      end
+      if current_page?(compute_path(element))
+        self.content_tag("li", content, class: "active")
+      else
+        self.content_tag("li", content)
+      end
     end
-  end
-  def compute_path(element)
-    case path = element.path
-    when Symbol
-      self.send(path)
-    when Proc
-      path.call(self)
-    else
-      self.url_for(path)
+
+    def compute_name(element)
+      case name = element.name
+      when Symbol
+        self.send(name)
+      when Proc
+        name.call(self)
+      else
+        name.to_s
+      end
     end
-  end
+
+    def compute_path(element)
+      case path = element.path
+      when Symbol
+        self.send(path)
+      when Proc
+        path.call(self)
+      else
+        self.url_for(path)
+      end
+    end
 
 end
--- app/helpers/weixin_message_reply_helper.rb
+++
@@ -1,3 +0,0 @@
-module WeixinMessageReplyHelper
-  
-end
