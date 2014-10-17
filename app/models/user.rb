2b82aea18473f5fcf218047847d162ed9e83694b
yinsigan <ganweiliang886@gmail.com>
2014-10-17 17:42:04 +0800

    15分钟之后超时

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :timeoutable
   has_many :public_accounts
 
   def admin?
     CONFIG['admin_emails'] && CONFIG['admin_emails'].include?(email)
+  end
+
+  def timeout_in
+    15.minutes
   end
 end
