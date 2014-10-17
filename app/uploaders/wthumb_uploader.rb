90b2f4bda590926843af8bbcacb58e1f0f6f252f
yinsigan <ganweiliang886@gmail.com>
2014-10-17 11:02:02 +0800

    图片预览

--- app/uploaders/wthumb_uploader.rb
+++ app/uploaders/wthumb_uploader.rb
@@ -4,7 +4,7 @@ class WthumbUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
@@ -42,9 +42,9 @@ class WthumbUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
