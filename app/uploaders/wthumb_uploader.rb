5ab7080944fde7d603bd0d396af051f7918acddc
yinsigan <ganweiliang886@gmail.com>
2014-10-17 11:32:38 +0800

    让图片存在七牛云

--- app/uploaders/wthumb_uploader.rb
+++ app/uploaders/wthumb_uploader.rb
@@ -11,7 +11,7 @@ class WthumbUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :qiniu
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
