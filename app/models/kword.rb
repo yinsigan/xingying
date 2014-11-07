df10bfd4938c88dd66c9980f23f6377af515b0b4
yinsigan <ganweiliang886@gmail.com>
2014-11-07 17:22:34 +0800

    修改显示图片bug

--- app/models/kword.rb
+++ app/models/kword.rb
@@ -7,6 +7,7 @@ class Kword < ActiveRecord::Base
   validates :public_account, presence: true
   validates :reply, presence: true, length: { maximum: 300 }, if: "subjectable_type == 'TextMaterial'"
   validates :subjectable_id, presence: true, if: "subjectable_type == 'SinMaterial'"
+  validates :subjectable_type, inclusion: { in: %w(SinMaterial TextMaterial) }
 
   ReplyType = {"TextMaterial" => "文字", "SinMaterial" => "单图文"}
   ReplyTypeNode = {"TextMaterial" => "text_material", "SinMaterial" => "sin_material"}
