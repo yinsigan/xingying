18b600fde70a03702aa385371456670a6687d487
yinsigan <ganweiliang886@gmail.com>
2014-11-13 16:55:00 +0800

    文章加上标题

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,7 @@ class ArticlesController < ApplicationController
   layout "article"
   def show
     @article = SinPicText.find(params[:id])
+    set_page_title @article.title
+    set_page_description @article.desc
   end
 end
