ac0dd3f1070a9279b468f97eccd0b53bdc5a1f16
AlanViast <Alan9510@163.com>
2014-11-18 11:39:11 +0800

    新增首頁3D切換效果

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,3 +32,6 @@
 //= require jquery.history
 //= require ckeditor/init
 //= require ckeditor/config
+//= require cute-slider/modernizr
+//= require cute-slider/cute.slider
+//= require cute-slider/cute.transitions.all
