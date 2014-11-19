6bf02540195b79a54a974489e435cb7e6f9dc0fc
yinsigan <ganweiliang886@gmail.com>
2014-11-19 15:39:16 +0800

    添加admin rails i18n

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,16 +33,16 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:   "smtp.gmail.com",
-  #   port:  587,
-  #   domain: "domain.of.sender.net",
-  #   authentication: "plain",
-  #   user_name: ENV["GMAIL_USERNAME"],
-  #   password: ENV["GMAIL_PASSWORD"],
-  #   enable_starttls_auto: true
-  # }
+  config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:   "smtp.gmail.com",
+    port:  587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
+  }
 end
