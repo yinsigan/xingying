132b28d2cc414513fac3a24e678c3af89f6582cb
yinsigan <ganweiliang886@gmail.com>
2014-11-22 11:00:32 +0800

    帮助

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,17 +1,20 @@
 class SupportsController < ApplicationController
+  before_action :find_all_category, only: [:index, :show]
+
   def index
-    @support_categories = SupportCategory.all
-    if params[:support_category_id]
-      @support_category = SupportCategory.find(params[:support_category_id])
-      @supports = @support_category.supports
-    else
-      @support_category = @support_categories.last
-      @supports = @support_category.supports
-    end
+    @support_category = params[:support_category_id].present? ? SupportCategory.find_by(id: params[:support_category_id]) : SupportCategory.first
+
+    @supports = @support_category.supports unless @support_category.blank?
   end
 
   def show
-    @support_categories = SupportCategory.all
     @support = Support.find(params[:id])
+  end
+
+  private
+
+  def find_all_category
+    set_page_title I18n.t('seo.support.index')
+    @support_categories = SupportCategory.all
   end
 end
