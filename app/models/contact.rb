3316422137d7f92d32d2d74b658a1f51054c91b9
yinsigan <ganweiliang886@gmail.com>
2014-11-24 18:08:48 +0800

    contact添加处理状态

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,6 @@ class Contact < ActiveRecord::Base
   validates :name, :body, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  STATUS = { 0: "未处理", 1: "正在处理"， 2: "已处理完毕" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124082022) do
+ActiveRecord::Schema.define(version: 20141124100649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141124082022) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status",     default: 0
   end
 
   create_table "kwords", force: true do |t|
