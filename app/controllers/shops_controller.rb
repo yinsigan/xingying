86a717a9e37d930f4a14318f5ad1f73b3ccdc3e6
AlanViast <Alan9510@163.com>
2014-12-03 18:18:36 +0800

    商品展示页面

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,20 @@
 class ShopsController < ApplicationController
-  layout "shops"
+  layout :select_layout
   def index
+
   end
+
+  def show
+
+  end
+
+  private
+    def select_layout
+      case action_name
+      when "index"
+        "shops"
+      else ""
+        "goods"
+      end
+    end
 end
