c92db5a59a23c231393c0accde2d88afd1bf6dd3
yinsigan <ganweiliang886@gmail.com>
2014-12-05 15:35:37 +0800

    用户管理增加面包屑

--- app/controllers/weixin_users_controller.rb
+++ app/controllers/weixin_users_controller.rb
@@ -1,6 +1,7 @@
 class WeixinUsersController < SettingsController
   before_action :set_public_account, :appid_present
   def index
+    add_breadcrumb I18n.t("breadcrumbs.weixin_user.index"), :public_account_weixin_users_path
   end
 
   def request_users
