acd09da398fe241ebdf45abc1904d388ded30c58
yinsigan <ganweiliang886@gmail.com>
2014-12-18 11:05:45 +0800

    sidekiq

--- app/workers/create_ticket_admin_notice_worker.rb
+++ app/workers/create_ticket_admin_notice_worker.rb
@@ -2,11 +2,10 @@ class CreateTicketAdminNoticeWorker
   include Sidekiq::Worker
 
   def perform(ticket_id)
-    ticket = ::Ticket.find_by(id: ticket_id)
-    return false if ticket.blank?
-    need_be_noticed_users = ::User.where.not(:role => 0)
+    ticket = Ticket.find(ticket_id)
+    need_be_noticed_users = User.where.not(:role => 0)
     need_be_noticed_users.each do |user|
-      ::Notification.create(
+      Notification.create(
         :messageable => ticket,
         :tp => 1,
         :subject => "您收到一条服务单<a href='/tickets/#{ticket.id}'>#{ticket.title}</a>",
