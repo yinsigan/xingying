bfc44525baaee7eac63974b19c36fd93a0a28711
suifeng2012 <ganweiliang886@gmail.com>
2015-03-30 12:59:08 +0800

    add mina-slack

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ require 'mina/rails'
 require 'mina/git'
 require 'mina/rbenv'
 require 'mina_sidekiq/tasks'
+require 'mina/slack'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
@@ -19,6 +20,11 @@ set :deploy_to, '/home/yinsigan/xy'
 set :repository, 'git@bitbucket.org:yinsigan/xingying.git'
 set :branch, 'master'
 set :term_mode, nil
+
+set :slack_token, "NTgtvGZSrn31cwClEJO5GIZc" # comes from inbound webhook integration
+set :slack_room, "#ruby" # the room to send the message to
+set :slack_subdomain, "qiangu" # if your subdomain is example.slack.com
+
 
 # For system-wide RVM install.
 #   set :rvm_path, '/usr/local/rvm/bin/rvm'
