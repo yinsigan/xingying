ddc08607384d0574d392544e5b47fe8ecf2303b4
suifeng2012 <ganweiliang886@gmail.com>
2015-04-21 15:20:07 +0800

    fix pid

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,7 +2,6 @@ app_path = File.expand_path( File.join(File.dirname(__FILE__), '..', '..'))
 worker_processes   1
 timeout            180
 listen             '/tmp/unicorn.xy.sock'
-pid                "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path        "log/unicorn.log"
 stdout_path        "log/unicorn.log"
 
