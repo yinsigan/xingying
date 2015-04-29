84a53fa35bdb92dc0b6f73e06125f4fe415f7081
suifeng2012 <ganweiliang886@gmail.com>
2015-04-29 12:10:09 +0800

    remove request log analyzer

--- config/deploy.rb
+++ config/deploy.rb
@@ -109,8 +109,3 @@ desc "Display the redis statues information"
 task :redis_stats do
   queue 'redis-cli info stats'
 end
-
-desc "Using request-log-analyzer display production log"
-task :request_log_analyzer do
-  queue "cd #{deploy_to}/#{current_path} && BUNDLE_GEMFILE=#{bundle_gemfile} #{bundle_prefix} request-log-analyzer #{deploy_to}/current/log/production.log"
-end
