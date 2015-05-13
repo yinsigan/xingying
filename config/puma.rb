f6a1f90fd5453a2bbfa73b725d36f1733618472e
suifeng2012 <ganweiliang886@gmail.com>
2015-05-13 13:21:48 +0800

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -18,8 +18,8 @@ bind "unix://#{shared_dir}/sockets/puma.sock"
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+# pidfile "#{shared_dir}/pids/puma.pid"
+# state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
