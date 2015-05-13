9857665ffeb482bf4414945ba0be75da888b5d17
suifeng2012 <ganweiliang886@gmail.com>
2015-05-13 12:58:04 +0800

    staging deploy file

--- config/puma/staging.rb
+++ config/puma/staging.rb
@@ -1,29 +1,27 @@
+app_path = File.expand_path( File.join(File.dirname(__FILE__), '..', '..'))
 # Change to match your CPU core count
 workers 1
 
 # Min and Max threads per worker
 threads 1, 6
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{app_path}/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{app_path}/log/puma.stdout.log", "#{app_path}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{app_path}/pids/puma.pid"
+state_path "#{app_path}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_path}/config/database.yml")[rails_env])
 end
