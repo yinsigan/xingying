eff4bdf318b78d5d7b22dc5ba86348d74510391b
suifeng2012 <ganweiliang886@gmail.com>
2015-05-14 09:44:17 +0800

    master unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,5 @@ set :repository, 'git@bitbucket.org:yinsigan/xingying.git'
 set :branch, 'master'
 set :term_mode, nil
 set :rails_env, 'production'
-set :puma_config,    -> { "#{deploy_to}/#{current_path}/config/puma/production.rb" }
+set :unicorn_config, -> { "#{deploy_to}/#{current_path}/config/unicorn/production.rb" }
 set :bundle_gemfile, "#{deploy_to}/#{current_path}/Gemfile"
