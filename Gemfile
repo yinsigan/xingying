bc3227d8c04cb900e808293f2571e9b17066e1e7
suifeng <ganweiliang886@gmail.com>
2015-06-28 22:52:52 +0800

    fix filter

--- Gemfile
+++ Gemfile
@@ -123,5 +123,6 @@ gem 'puma', '~> 2.11.2'
 gem 'mina-puma', require: false
 gem 'devise_security_extension'
 gem 'easy_captcha'
+# alt+p hotkey
 gem 'rack-mini-profiler', require: false
 gem 'simplecov', :require => false, :group => :test
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # reset captcha code after each request for security
   after_filter :reset_last_captcha_code!
-  before_action :authorize
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -15,9 +14,4 @@ class ApplicationController < ActionController::Base
     redirect_to main_app.root_path, :alert => t('no_permission')
   end
 
-  def authorize
-    if current_user.super_admin?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
 end
